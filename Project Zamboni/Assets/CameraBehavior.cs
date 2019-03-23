686ee611337a15621b65a44352cf9ac040c21e66
David Henshaw <davidhenshaw2014@gmail.com>
2019-03-23 11:12:26 -0500

    Create camera-follow, ground tiles, and wall collision

---
+++ Project Zamboni/Assets/CameraBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GameObject playerGO = GameObject.FindWithTag("Player");
+        //playerTransform = (Transform)playerGO.GetComponent("Transform");
+        offset.z = -3;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}
